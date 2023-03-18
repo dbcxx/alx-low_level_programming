96254fe39bbe8394f8025ad398b695210702477d
Dolapo <bamideledolapo.s@gmail.com>
2023-03-17 17:01:13 -0700

    rerc

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,15 @@
  */
 int main(void)
 {
+	char letter = 'a';
 
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-		putchar('\n');
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
 
 return (0);
 }
